8adb9d9bb48b8c0c403141cd7f9e1ddf2bca0fa2
ayushporwal01 <porwalayush953@gmail.com>
2026-04-03 12:25:02 +0530

    Initial commit

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
 const app = React.createElement("div", {}, [ 
+    React.createElement("h1", {id: "heading"}, "Hello World From React!"),
     React.createElement("div", {id: "parent"}, 
         React.createElement("div", {id:"child"}, 
             React.createElement("h1", {}, "")
