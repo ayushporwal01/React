4ad8d2f3acdbad6be7a4ab2c7c2564fb75892348
ayushporwal01 <porwalayush953@gmail.com>
2026-04-03 12:35:14 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id:"child"}, [ 
+    React.createElement("div", {id:"child1"}, [ 
         React.createElement("h1", {}, "I'm an h1 tag"),
         React.createElement("h2", {}, "I'm an h2 tag")
     ]),
 
-    React.createElement("div", {id:"child"}, [ 
+    React.createElement("div", {id:"child2"}, [ 
         React.createElement("h1", {}, "I'm another h1 tag"),
         React.createElement("h2", {}, "I'm another h2 tag")
     ])
