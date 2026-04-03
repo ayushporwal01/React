2803b7b21f908d7548448688d46bcc7333859ab5
ayushporwal01 <porwalayush953@gmail.com>
2026-04-03 12:31:51 +0530

    Initial commit

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ const app = React.createElement("div", {},
         React.createElement("div", {id:"child"}, [ 
             React.createElement("h1", {}, "I'm an h1 tag"),
             React.createElement("h2", {}, "I'm an h2 tag")
-        ]
+            ]
         )
     )
-]);
+);
 
 root.render(app)
