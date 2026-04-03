66caa6ad2646afe6b0128ecab03cc97a46184eb5
ayushporwal01 <porwalayush953@gmail.com>
2026-04-03 12:23:31 +0530

    Initial commit

--- App.js
+++ App.js
@@ -2,10 +2,9 @@ const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello Wo
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
-const parent = React.createElement("div", 
-    {id: "parent"}, 
-    React.createElement("div", 
-        {id:"child"}, 
+const app = 
+    React.createElement("div", {id: "parent"}, 
+    React.createElement("div", {id:"child"}, 
         React.createElement("h1", {}, "")
     )
 )
