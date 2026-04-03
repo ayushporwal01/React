5513b79126f181bc755547ac06d11d98d13c7a88
ayushporwal01 <porwalayush953@gmail.com>
2026-04-03 12:34:20 +0530

    Initial commit

--- App.js
+++ App.js
@@ -4,6 +4,11 @@ const parent = React.createElement("div", {id: "parent"},
     React.createElement("div", {id:"child"}, [ 
         React.createElement("h1", {}, "I'm an h1 tag"),
         React.createElement("h2", {}, "I'm an h2 tag")
+    ]),
+
+    React.createElement("div", {id:"child"}, [ 
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")
     ])
 );
 
