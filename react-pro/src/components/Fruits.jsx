2b9739ab15f12363e7368ee0f86ec7cd9b4391a0
ayushporwal01 <porwalayush953@gmail.com>
2026-04-11 20:13:49 +0530

    Initial commit

--- react-pro/src/components/Fruits.jsx
+++ react-pro/src/components/Fruits.jsx
@@ -1,13 +1,11 @@
 export default function Fruits() {
   const Fruits = [
-    {name: "Apple", price: 10, emoji: "🍎"},
-    {name: "Mango", price: 7, emoji: "🥭"},
-    {name: "Orange", price: 5, emoji: "🍊"},
-    {name: "Banana", price: 2, emoji: "🍌"},
-    {name: "Pineapple", price: 8, emoji: "🍍"}
-  ]
-  
-  return (
-  <div></div>
-  );
+    { name: "Apple", price: 10, emoji: "🍎" },
+    { name: "Mango", price: 7, emoji: "🥭" },
+    { name: "Orange", price: 5, emoji: "🍊" },
+    { name: "Banana", price: 2, emoji: "🍌" },
+    { name: "Pineapple", price: 8, emoji: "🍍" },
+  ];
+
+  return <div></div>;
 }
