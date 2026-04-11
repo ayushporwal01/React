51e90c248bd9b2442100e505e4def3f3e51be045
ayushporwal01 <porwalayush953@gmail.com>
2026-04-11 11:06:24 +0530

    Initial commit

--- react-pro/src/App.jsx
+++ react-pro/src/App.jsx
@@ -1,9 +1,15 @@
 import Hello from './components/Hello';
 
 function App() {
+  const person = {
+    name: "Harry",
+    message: "Hi There!",
+    emoji: "👋",
+    seatNumbers: [1, 4, 7] 
+  }
   return (
     <div className="App">
-      <Hello name="Ayush" message="Hi There!" emoji="👋" />
+      <Hello  />
     </div>
   )
 }
