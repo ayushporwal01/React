7af81f719066e630a0ebc03214aa4e0ee7227411
ayushporwal01 <porwalayush953@gmail.com>
2026-04-12 10:12:04 +0530

    Initial commit

--- react-pro/src/ConditionalComponent.jsx
+++ react-pro/src/ConditionalComponent.jsx
@@ -1,3 +1,6 @@
+import Welcome from "./components/Welcome";
+import Code from "./components/Code";
+
 export default function ConditionalComponent() {
   const display = false;
   if (display) return <Welcome />;
