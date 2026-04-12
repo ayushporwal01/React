12abae917b0019a044b17ae4bca335203309b230
ayushporwal01 <porwalayush953@gmail.com>
2026-04-12 11:13:36 +0530

    Initial commit

--- react-pro/src/components/Fruit.jsx
+++ react-pro/src/components/Fruit.jsx
@@ -1,13 +1,9 @@
 export default function Fruit({ name, price, emoji }) {
   return (
     <>
-      {price > 5 ? (
-        <li>
-          {emoji} {name} ${price}
-        </li>
-      ) : (
-        ""
-      )}
+      <li>
+        {emoji} {name} ${price}
+      </li>
     </>
   );
 }
