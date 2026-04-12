31d65f02fe21b4fa369aea1bf0bff9d4077b8467
ayushporwal01 <porwalayush953@gmail.com>
2026-04-12 10:13:16 +0530

    Initial commit

--- react-pro/src/App.jsx
+++ react-pro/src/App.jsx
@@ -1,11 +1,11 @@
-import ConditionalComponent from "./ConditionalComponent"
+import ConditionalComponent from "./components/ConditionalComponent";
 
 function App() {
   return (
     <div className="App">
       <ConditionalComponent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-pro/src/components/ConditionalComponent.jsx
+++ react-pro/src/components/ConditionalComponent.jsx
@@ -1,5 +1,5 @@
-import Welcome from "./components/Welcome";
-import Code from "./components/Code";
+import Welcome from "./Welcome";
+import Code from "./Code";
 
 export default function ConditionalComponent() {
   const display = true;
