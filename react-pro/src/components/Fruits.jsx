a323153e1f957e7d4bcaf751ecdcbb8366c9efc2
ayushporwal01 <porwalayush953@gmail.com>
2026-04-12 09:53:58 +0530

    Initial commit

--- react-pro/src/components/Fruits.jsx
+++ react-pro/src/components/Fruits.jsx
@@ -1,3 +1,5 @@
+import Fruit from "./Fruit";
+
 export default function Fruits() {
   const fruits = [
     { name: "Apple", price: 10, emoji: "🍎" },
@@ -11,9 +13,7 @@ export default function Fruits() {
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit.name}>
-            {fruit.name} ${fruit.price} {fruit.emoji}
-          </li>
+          <Fruit />
         ))}
       </ul>
     </div>
