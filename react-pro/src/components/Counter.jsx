58e8511dd559fa9d7f2e72f3adac01f86af99378
ayushporwal01 <porwalayush953@gmail.com>
2026-04-13 11:23:35 +0530

    Initial commit

--- react-pro/src/components/Counter.jsx
+++ react-pro/src/components/Counter.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
   return <div>
     <h1>count is:</h1>
     <button onClick={increment}>Increment</button>
