bc3b12aad4a407a33c35f680e882c797c805286e
ayushporwal01 <porwalayush953@gmail.com>
2026-04-13 11:23:09 +0530

    Initial commit

--- react-pro/src/components/Counter.jsx
+++ react-pro/src/components/Counter.jsx
@@ -4,5 +4,6 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   return <div>
     <h1>count is:</h1>
+    <button onClick={increment}>Increment</button>
   </div>;
 }
