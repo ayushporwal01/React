a15152eae08105fb1afcdbbfd2fdeba1dd7b19d2
ayushporwal01 <porwalayush953@gmail.com>
2026-04-13 10:33:56 +0530

    Initial commit

--- react-pro/src/components/Message.jsx
+++ react-pro/src/components/Message.jsx
@@ -1,7 +1,10 @@
 export default function Message() {
   return (
+    function handleClick() {
+        console.log("Button Clicked!")
+    }
     <div>
-      <button>Click here to get message</button>
+      <button onClick={handleClick}>Click here to get message</button>
     </div>
   );
 }
