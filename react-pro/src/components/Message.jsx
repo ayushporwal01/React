20bf320c317b396f8d6e6345aa07c333c0690281
ayushporwal01 <porwalayush953@gmail.com>
2026-04-13 10:29:44 +0530

    Initial commit

--- react-pro/src/components/Message.jsx
+++ react-pro/src/components/Message.jsx
@@ -1,5 +1,7 @@
 export default function Message() {
-    return <div>
-        <button>Click here to get message</button>
+  return (
+    <div>
+      <button>Click here to get message</button>
     </div>
+  );
 }
