4a834e6cec121e7bad5c3e2a48cd72abe0a4315e
ayushporwal01 <porwalayush953@gmail.com>
2026-04-13 19:10:10 +0530

    Initial commit

--- react-pro/src/components/Form.jsx
+++ react-pro/src/components/Form.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Form() {
-    const [name, setName] = useState("");
-    return (
-        <div>
-            <form>
-                <input type="text" />
-            </form>
-        </div>
-    )
+  const [name, setName] = useState("");
+  return (
+    <div>
+      <form>
+        <input type="text" onChange={(e) => e.target.value} value={name} />
+      </form>
+    </div>
+  );
 }
