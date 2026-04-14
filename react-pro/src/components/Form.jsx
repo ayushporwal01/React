a7e38b0b408d34b77672a4ad3dd2d04b155734b0
ayushporwal01 <porwalayush953@gmail.com>
2026-04-14 12:00:06 +0530

    Initial commit

--- react-pro/src/components/Form.jsx
+++ react-pro/src/components/Form.jsx
@@ -20,6 +20,7 @@ export default function Form() {
           type="text"
           value={name.lastName}
         />
+        <br/>
         <button onClick={handleSubmit}>Submit</button>
       </form>
     </div>
