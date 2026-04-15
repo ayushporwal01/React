bfe3be186ccae533887300f4a0da71012a41a76c
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 10:18:02 +0530

    Initial commit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -5,7 +5,7 @@ export default function Todo() {
   return (
     <div>
       <form>
-        <input type="text"/>
+        <input type="text" value={Todo} />
         <button type="submit">Add</button>
       </form>
     </div>
