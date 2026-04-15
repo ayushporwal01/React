0afee58f1b9eb0bd1e9787f660af82017c5a7607
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 10:28:12 +0530

    Initial commit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function Todo() {
   const [Todo, setTodo] = useState("");
+  const [Todos, setTodos] = useState([]);
   function handleSubmit(e) {
     e.preventDefault();
   }
