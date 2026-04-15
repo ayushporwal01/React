f52190663f1d4a63d5ed4b9645f4363f3ab3edce
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 11:02:10 +0530

    Initial commit

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,8 +1,9 @@
+import InlineComponent from "./components/InlineComponent";
 import Todo from "./components/Todo";
 
 function App() {
   return <div className="App">
-    <Todo/>
+    <InlineComponent/>
   </div>;
 }
 
