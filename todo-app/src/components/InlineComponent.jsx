950fe8f3f39186b06a7458e824e25cd29869c82b
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 11:03:07 +0530

    Initial commit

--- todo-app/src/components/InlineComponent.jsx
+++ todo-app/src/components/InlineComponent.jsx
@@ -1,3 +1,5 @@
+const header = { color: "red", fontSize: "140px" };
+
 export default function InlineComponent() {
-    return <h1>Inline Component</h1>
+  return <h1 style={{ header }}>Inline Component</h1>;
 }
