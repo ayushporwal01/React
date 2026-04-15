515c4628115dea9f443db55e57f1340855caaad0
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 10:48:14 +0530

    Initial commit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -22,7 +22,7 @@ export default function Todo() {
 
       <ul>
         {Todos.map((item) => (
-          <TodoItem item={item} />
+          <TodoItem item={item.text} />
         ))}
       </ul>
     </div>
