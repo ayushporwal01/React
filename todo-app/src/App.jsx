fa6f125762fcf20d5e4f838a4db624abe16bc2c1
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 21:45:54 +0530

    Initial commit

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,12 +1,9 @@
-import InlineComponent from "./components/InlineComponent";
-import OutlineComponent from "./components/OutlineComponent";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
-      <InlineComponent />
-      <OutlineComponent />
+      <Todo/>
     </div>
   );
 }
--- todo-app/src/components/InlineComponent.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./InlineComponent.module.css";
-
-export default function InlineComponent() {
-  return <h1 className={styles.header}>Inline Component</h1>;
-}
