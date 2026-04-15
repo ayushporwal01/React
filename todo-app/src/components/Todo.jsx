e2cb8d1bf5be074f0105f119223b9f7acb83f129
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 10:11:10 +0530

    Initial commit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 export default function Todo() {
-    return <div>
-        <form>
-            <input type="text" />
-            <button type="submit">Add</button>
-        </form>
+  const [Todo, setTodo] = useState("");
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <button type="submit">Add</button>
+      </form>
     </div>
+  );
 }
