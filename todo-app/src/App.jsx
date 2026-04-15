ea714bff311f4ef645068b34bdf9d04ea78d50f2
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 11:45:45 +0530

    Initial commit

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,9 +1,11 @@
 import InlineComponent from "./components/InlineComponent";
+import OutlineComponent from "./components/OutlineComponent";
 import Todo from "./components/Todo";
 
 function App() {
   return <div className="App">
     <InlineComponent/>
+    <OutlineComponent/>
   </div>;
 }
 
