afd7cbbb8943f08805c51416f3bdf22c897bda3e
ayushporwal01 <porwalayush953@gmail.com>
2026-04-15 10:19:03 +0530

    Initial commit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -5,7 +5,11 @@ export default function Todo() {
   return (
     <div>
       <form>
-        <input onChange={(e) => setTodo(e.target.value)} type="text" value={Todo} />
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          value={Todo}
+        />
         <button type="submit">Add</button>
       </form>
     </div>
