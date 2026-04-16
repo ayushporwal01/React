2cb73848b85aa5895db097b54224e213dfe9b056
ayushporwal01 <porwalayush953@gmail.com>
2026-04-16 19:12:27 +0530

    Initial commit

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 import styles from "./todoItem.module.css";
 
 export default function TodoItem({ item, todos, setTodos }) {
-  function deleteTask(item, todos, setTodos) {
+  function deleteTask(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
   return (
