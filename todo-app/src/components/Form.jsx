77677c4bf68c3f4f80a24975eecbd15f4b2c9bec
ayushporwal01 <porwalayush953@gmail.com>
2026-04-16 09:15:58 +0530

    Initial commit

--- todo-app/src/components/Form.jsx
+++ todo-app/src/components/Form.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
-
 export default function Form() {
-  const [Todo, setTodo] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...Todos, Todo]);
