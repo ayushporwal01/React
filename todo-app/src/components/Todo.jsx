c9fa1205cd09df618476f9e9cdb94cbfc5fdba8d
ayushporwal01 <porwalayush953@gmail.com>
2026-04-16 09:24:23 +0530

    Initial commit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Form from "./Form";
 import TodoList from "./TodoList";
-import TodoItem from "./TodoItem";
 
 export default function Todo() {
   const [Todos, setTodos] = useState([]);
