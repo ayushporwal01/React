cccaf048f15b30c06852135c7c49b22768c70f0e
ayushporwal01 <porwalayush953@gmail.com>
2026-04-16 09:15:10 +0530

    Initial commit

--- todo-app/src/components/Form.jsx
+++ todo-app/src/components/Form.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [Todo, setTodo] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...Todos, Todo]);
--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import TodoItem from "./TodoItem";
 
 export default function Todo() {
-  const [Todo, setTodo] = useState("");
+
   const [Todos, setTodos] = useState([]);
  
   return (
