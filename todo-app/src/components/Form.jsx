8284a7df0fefee7ce407bde3006487ad9c44ee01
ayushporwal01 <porwalayush953@gmail.com>
2026-04-16 09:47:15 +0530

    Initial commit

--- todo-app/src/components/Form.jsx
+++ todo-app/src/components/Form.jsx
@@ -3,11 +3,6 @@ import styles from "./form.module.css";
 
 export default function Form({ todos, setTodos }) {
   const [todo, setTodo] = useState("");
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo("");
-  }
   return (
     <form onSubmit={handleSubmit}>
       <input
