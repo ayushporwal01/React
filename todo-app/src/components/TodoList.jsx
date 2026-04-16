3b0cdb121006da7b716cfc4b4b03633ce6defc16
ayushporwal01 <porwalayush953@gmail.com>
2026-04-16 17:18:05 +0530

    Initial commit

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -2,7 +2,7 @@ import TodoItem from "./TodoItem";
 
 export default function TodoList({ todos }) {
   return (
-    <div>
+    <div className={styles.list}>
       <ul>
         {todos.map((item, index) => (
           <TodoItem key={index} item={item} />
