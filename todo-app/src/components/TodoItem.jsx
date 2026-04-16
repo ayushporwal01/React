5287ff52397c0e18fe193a5fb507b0d33a1e9cd5
ayushporwal01 <porwalayush953@gmail.com>
2026-04-16 18:44:55 +0530

    Initial commit

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -1,7 +1,12 @@
 import styles from "./todoItem.module.css";
 
 export default function TodoItem({ item }) {
-  return <li className={styles.item}>{item}
-  
-  </li>;
+  return (
+    <li className={styles.item}>
+      {item}
+      <span>
+        <button className={styles.deleteBtn}>X</button>
+      </span>
+    </li>
+  );
 }
