bdccce863c129ea4bc4df0b5819becd10fa75f96
ayushporwal01 <porwalayush953@gmail.com>
2026-04-17 12:50:16 +0530

    Initial commit

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -4,9 +4,11 @@ export default function TodoItem({ item, todos, setTodos }) {
   function deleteTask(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
+
+  function 
   return (
     <div className={styles.item}>
-      <li>{item.name}</li>
+      <li onClick={completeTask(item.name)}>{item.name}</li>
       <button onClick={(e) => deleteTask(item)} className={styles.deleteBtn}>
         x
       </button>
