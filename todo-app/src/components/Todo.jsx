2241bbf474c47d56b8df6bb01c72736384218a89
ayushporwal01 <porwalayush953@gmail.com>
2026-04-17 13:16:52 +0530

    Initial commit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -4,11 +4,13 @@ import TodoList from "./TodoList";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
- 
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+
   return (
     <div>
       <Form todos={todos} setTodos={setTodos} />
       <TodoList todos={todos} setTodos={setTodos} />
+      <h1>{completedTodos}</h1>
     </div>
   );
 }
