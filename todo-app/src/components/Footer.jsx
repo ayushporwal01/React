217ade519bd0328dddf8c843f38965b15fa0139b
ayushporwal01 <porwalayush953@gmail.com>
2026-04-17 13:41:43 +0530

    Initial commit

--- todo-app/src/components/Footer.jsx
+++ todo-app/src/components/Footer.jsx
@@ -3,7 +3,8 @@ import styles from "./footer.module.css";
 export default function Footer({ completedTodos }) {
   return (
     <div className={styles.footer}>
-        <span className={styles.footerText}>Completed Todos: {completedTodos}</span>
+        <span className={styles.completedTodos}>Completed Todos: {completedTodos}</span>
+        <span className={styles.totalTodos}>Total Todos: {totalTodos}</span>
     </div>
   );
 }
