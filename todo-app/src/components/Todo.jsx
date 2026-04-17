e2716227892499a6b7aa7320aa2daaa1e59a31f7
ayushporwal01 <porwalayush953@gmail.com>
2026-04-17 13:40:00 +0530

    Initial commit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -6,12 +6,13 @@ import Footer from "./Footer";
 export default function Todo() {
   const [todos, setTodos] = useState([]);
   const completedTodos = todos.filter((todo) => todo.completed).length;
+  const totalTodos = todos.length;
 
   return (
     <div>
       <Form todos={todos} setTodos={setTodos} />
       <TodoList todos={todos} setTodos={setTodos} />
-      <Footer completedTodos={completedTodos} />
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos}  />
     </div>
   );
 }
