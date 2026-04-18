8e2e2c42b96c09343584b4db723734cd24c4e5cc
ayushporwal01 <porwalayush953@gmail.com>
2026-04-18 22:13:36 +0530

    Initial commit

--- food-app/src/components/Header.jsx
+++ food-app/src/components/Header.jsx
@@ -2,6 +2,13 @@ export default function Header() {
   return (
     <div>
       <img src="./assets/food-logo.webp" alt="Namaste Food Logo" />
+      <div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 }
