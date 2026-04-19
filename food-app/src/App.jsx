5247905844dcd2dcdf7f85844dda706cd2b4675c
ayushporwal01 <porwalayush953@gmail.com>
2026-04-19 10:42:07 +0530

    Initial commit

--- food-app/src/App.jsx
+++ food-app/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Body from "./components/Body";
 import "./App.css";
 
 function App() {
-  return <Header />;
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
--- food-app/src/components/Body.jsx
+++ food-app/src/components/Body.jsx
@@ -1,11 +1,19 @@
 export default function Body() {
-    return <div className="body">
-        <div className="searchContainer">
-            <input className="searchBar" type="text" placeholder="Enter a Restaurant Name..." />
-            <button className="searchBtn"></button>
-        </div>
-        <div className="ResContainer">
+  return (
+    <div className="body">
+      <div className="searchContainer">
+        <input
+          className="searchBar"
+          type="text"
+          placeholder="Enter a Restaurant Name..."
+        />
+        <button className="searchBtn">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+      {/* <div className="ResContainer">
             <RestaurantCards/>
-        </div>
+        </div> */}
     </div>
+  );
 }
