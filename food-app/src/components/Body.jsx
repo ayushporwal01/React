9854aa081b9b0da496a2eda2ece7eddd308ad054
ayushporwal01 <porwalayush953@gmail.com>
2026-04-19 10:36:24 +0530

    Initial commit

--- food-app/src/components/Body.jsx
+++ food-app/src/components/Body.jsx
@@ -1,3 +1,11 @@
 export default function Body() {
-    return <div>Body</div>
+    return <div className="body">
+        <div className="searchContainer">
+            <input className="searchBar" type="text" placeholder="Enter a Restaurant Name..." />
+            <button className="searchBtn"></button>
+        </div>
+        <div className="ResContainer">
+            <RestaurantCards/>
+        </div>
+    </div>
 }
