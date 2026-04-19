c8a233fddfe5a5789634342cfa2689a3a1f306a6
ayushporwal01 <porwalayush953@gmail.com>
2026-04-19 23:20:37 +0530

    Initial commit

--- food-app/src/components/RestaurantCard.jsx
+++ food-app/src/components/RestaurantCard.jsx
@@ -1,23 +1,32 @@
 import styles from "./resCard.module.css";
 
-export default function RestaurantCard({resData, resName, rating, deliveryTime, cuisine, location}) {
+export default function RestaurantCard({ resData }) {
+  const { name, avgRating, cuisines, areaName, sla } = resData?.info;
+
+  const deliveryTime = sla?.deliveryTime;
+
+  const url =
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+  const id = resData.info.cloudinaryImageId;
+
   return (
     <div className={styles.resCard}>
       <div className={styles.resImage}>
-        <img src="src/assets/res-img/1.jpg" alt="Pizza Hut Image" />
+        <img src={url + id} alt={name} />
       </div>
       <div className={styles.resDetails}>
-        <h1 className={styles.resName}>{resData.name}</h1>
+        <h1 className={styles.resName}>{name}</h1>
 
         <span className={styles.extraDetails}>
           <span className={styles.starIcon}>&#9733;</span>
-          <span className={styles.rating}>{resData.avgRating}</span> &bull;
-          <span className={styles.deliveryTime}>{resData.deliveryTime}</span>
+          <span className={styles.rating}>{avgRating}</span> &bull;
+          <span className={styles.deliveryTime}>{deliveryTime} mins</span>
         </span>
-        
+
         <div className={styles.moreDetails}>
-            <p className={styles.cuisine}>{resData.cuisines}</p>    
-            <p className={styles.location}>{resData.areaName}</p>
+          <p className={styles.cuisine}>{cuisines.join(", ")}</p>
+          <p className={styles.location}>{areaName}</p>
         </div>
       </div>
     </div>
