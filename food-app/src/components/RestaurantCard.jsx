8d3a426f2bbdc9bd894fb3aa7ee7041275d8f760
ayushporwal01 <porwalayush953@gmail.com>
2026-04-19 19:30:52 +0530

    Initial commit

--- food-app/src/components/RestaurantCard.jsx
+++ food-app/src/components/RestaurantCard.jsx
@@ -7,17 +7,17 @@ export default function RestaurantCard({resData, resName, rating, deliveryTime, 
         <img src="src/assets/res-img/1.jpg" alt="Pizza Hut Image" />
       </div>
       <div className={styles.resDetails}>
-        <h1 className={styles.resName}>{resName}</h1>
+        <h1 className={styles.resName}>{resData.name}</h1>
 
         <span className={styles.extraDetails}>
           <span className={styles.starIcon}>&#9733;</span>
-          <span className={styles.rating}>{rating}</span> &bull;
-          <span className={styles.deliveryTime}>{deliveryTime}</span>
+          <span className={styles.rating}>{resData.avgRating}</span> &bull;
+          <span className={styles.deliveryTime}>{resData.deliveryTime}</span>
         </span>
         
         <div className={styles.moreDetails}>
-            <p className={styles.cuisine}>{cuisine}</p>    
-            <p className={styles.location}>{location}</p>
+            <p className={styles.cuisine}>{resData.cuisines}</p>    
+            <p className={styles.location}>{resData.areaName}</p>
         </div>
       </div>
     </div>
