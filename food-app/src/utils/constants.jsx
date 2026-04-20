f848efacc45ad01e438803234a60d98c6a8e8383
ayushporwal01 <porwalayush953@gmail.com>
2026-04-20 10:05:13 +0530

    Initial commit

--- food-app/src/utils/constants.jsx
+++ food-app/src/utils/constants.jsx
@@ -2,7 +2,3 @@ const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 const LOGO_URL = "src/assets/food-logo.png";
-
-const ID = resData.info.cloudinaryImageId;
-
-  const deliveryTime = sla?.deliveryTime;
