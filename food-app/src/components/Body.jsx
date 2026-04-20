4122eaf2507b7c43aa25268e873d2f646d4ad707
ayushporwal01 <porwalayush953@gmail.com>
2026-04-20 19:31:49 +0530

    Initial commit

--- food-app/src/components/Body.jsx
+++ food-app/src/components/Body.jsx
@@ -3,7 +3,7 @@ import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
 import SearchBar from "./SearchBar";
 import ItemFilter from "./ItemFilter";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Body() {
   const processedList = resList.map((res) => ({
@@ -12,6 +12,20 @@ export default function Body() {
   }));
 
   const [list, setList] = useState(processedList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  
+  async function fetchData() {
+    const data = await fetch(
+      "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=23.25050&lng=77.40650&carousel=true&third_party_vendor=1",
+    );
+    
+    const json = await data.json();
+    setList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+  }
 
   return (
     <div className={styles.body}>
