4c78437fb2cabcad825a6316653e1bd1845517d9
ayushporwal01 <porwalayush953@gmail.com>
2026-04-21 19:33:17 +0530

    Initial commit

--- food-app/src/components/Shimmer.jsx
+++ food-app/src/components/Shimmer.jsx
@@ -1,7 +1,15 @@
+import styles from "./shimmer.module.css";
+
 export default function Shimmer() {
   return (
-    <div>
-      <h1>Shimmer....</h1>
+    <div className={styles.shimmerContainer}>
+      <div className={styles.shimmerCard}>
+        <div className={styles.shimmerImg}></div>
+        <div className={shimmerLine1}></div>
+        <div className={shimmerLine2}></div>
+        <div className={shimmerLine3}></div>
+        <div className={shimmerLine4}></div>
+      </div>
     </div>
   );
 }
