5bafae7a14dc6ff1ab7c400d627bef956666f8be
ayushporwal01 <porwalayush953@gmail.com>
2026-05-07 09:24:34 +0530

    Initial commit

--- food-app/src/components/Shimmer.jsx
+++ food-app/src/components/Shimmer.jsx
@@ -1,12 +1,12 @@
 export default function Shimmer() {
   return (
-    <div className="w-full h-full p-3 border">
+    <div className="w-full h-full p-3">
       <div className="w-full h-45 bg-gray-200 rounded-xl animate-pulse"></div>
-      <div className="pl-2 pt-3 space-y-2">
-        <div className="w-64 h-5 bg-gray-200 rounded animate-pulse"></div>
-        <div className="w-40 h-5 bg-gray-200 rounded animate-pulse"></div>
-        <div className="w-64 h-5 bg-gray-200 rounded animate-pulse"></div>
-        <div className="w-20 h-5 bg-gray-200 rounded animate-pulse"></div>
+      <div className="pl-2 mt-4 space-y-2">
+        <div className="w-64 h-4.5 bg-gray-200 rounded animate-pulse"></div>
+        <div className="w-40 h-4.5 bg-gray-200 rounded animate-pulse"></div>
+        <div className="w-64 h-4.5 bg-gray-200 rounded animate-pulse"></div>
+        <div className="w-20 h-4.5 bg-gray-200 rounded animate-pulse"></div>
       </div>
     </div>
   );
