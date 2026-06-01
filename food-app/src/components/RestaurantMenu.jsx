a6936bf0de53dba796cd9811cd39f31d29867acc
ayushporwal01 <porwalayush953@gmail.com>
2026-06-01 14:59:41 +0530

    Initial commit

--- food-app/src/components/RestaurantMenu.jsx
+++ food-app/src/components/RestaurantMenu.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import Shimmer from "./Shimmer";
-import styles from "./resMenu.module.css";
 import Category from "./Category";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 
@@ -13,18 +12,22 @@ export default function RestaurantMenu() {
 
   if (resInfo === null) return <Shimmer />;
 
-  const categoryCards = resInfo?.cards?.find((c) => c?.groupedCard)?.groupedCard
-    ?.cardGroupMap?.REGULAR?.cards;
+  const categorySection = resInfo?.cards?.find((c) => c?.groupedCard)
+    ?.groupedCard?.cardGroupMap?.REGULAR?.cards;
 
-  let cardsWithTitle = categoryCards.filter((card) => card?.card?.card?.title);
+  let categoryCards = categorySection?.filter((c) => {
+    const card = c?.card?.card;
 
-  console.log(cardsWithTitle);
+    return (
+      card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory" &&
+      !card?.title?.toLowerCase().startsWith("items starting at")
+    );
+  });
 
-  if (cardsWithTitle[0]?.card?.card?.title?.toLowerCase() !== "recommended") {
-    cardsWithTitle = cardsWithTitle.slice(1);
-  }
+  console.log("category-cards", categoryCards);
 
-  const subCategoryCards = categoryCards.filter(
+  const subCategoryCards = categorySection?.filter(
     (card) => card?.card?.card?.categories,
   );
 
@@ -34,25 +37,25 @@ export default function RestaurantMenu() {
     infoCard?.card?.card?.info || {};
 
   return (
-    <div className={styles.menuContainer}>
-      <div className={styles.menu}>
-        <div className={styles.menuDetails}>
+    <div className="">
+      <div className="">
+        <div className="">
           <h1>{name}</h1>
 
-          <span className={styles.extraDetails}>
-            <span className={styles.starIcon}>&#9733;</span>
-            <span className={styles.rating}>{avgRating}</span> &bull;
-            <span className={styles.cost}> {costForTwoMessage}</span>
+          <span className="">
+            <span className="">&#9733;</span>
+            <span className="">{avgRating}</span> &bull;
+            <span className=""> {costForTwoMessage}</span>
           </span>
 
-          <p className={styles.cuisine}>{cuisines?.join(", ")}</p>
+          <p className="">{cuisines?.join(", ")}</p>
         </div>
 
-        <h4 className={styles.menuText}>Menu</h4>
+        <h4 className="">Menu</h4>
 
         {/* Menu Sections */}
-        <div className={styles.menuCategories}>
-          {cardsWithTitle?.map((section, index) => {
+        <div className="">
+          {categoryCards?.map((section, index) => {
             return (
               <Category
                 key={index}
