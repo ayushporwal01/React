8284f6f00b679a57e42cca7d73641b5ed9d683bc
ayushporwal01 <porwalayush953@gmail.com>
2026-06-01 15:56:00 +0530

    Initial commit

--- food-app/src/components/Header.jsx
+++ food-app/src/components/Header.jsx
@@ -2,8 +2,9 @@ import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import { Menu } from "react-feather";
 import SideBar from "./SideBar";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { AnimatePresence } from "motion/react";
+import userContext from "../utils/userContext";
 
 export default function Header() {
   const navLinks = [
@@ -17,6 +18,8 @@ export default function Header() {
   function toggleSidebar() {
     setIsSidebarOpen(!isSidebarOpen);
   }
+
+  const { loggedInUser } = useContext(userContext);
 
   return (
     <div className="w-full max-w-8xl h-18 px-10 md:px-20 lg:px-25 flex justify-between items-center border border-black bg-white sticky top-0 left-0 right-0 z-100">
@@ -32,6 +35,7 @@ export default function Header() {
             <Link to={link.path}>{link.name}</Link>
           </li>
         ))}
+        <li>{loggedInUser}</li>
       </ul>
       <button onClick={toggleSidebar} className="md:hidden cursor-pointer">
         <Menu size={35} />
