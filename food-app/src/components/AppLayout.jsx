0de2e19909f0ae382bee828ca44a0f575a2ef39d
ayushporwal01 <porwalayush953@gmail.com>
2026-06-04 22:22:40 +0530

    Initial Commit

--- food-app/src/components/AppLayout.jsx
+++ food-app/src/components/AppLayout.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import userContext from "../utils/userContext";
+import { Provider } from "react-redux";
+import appStore from "../utils/appStore";
 
 export default function AppLayout() {
   const [userName, setUserName] = useState();
@@ -16,11 +18,13 @@ export default function AppLayout() {
   }, []);
 
   return (
+    <Provider store={appStore}>
     <userContext.Provider value={{ loggedInUser: userName, setUserName }}>
       <>
         <Header />
         <Outlet />
       </>
     </userContext.Provider>
+    </Provider>
   );
 }
