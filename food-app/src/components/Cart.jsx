876e4146a90f5d57c0da207a61e9405acd2af97b
ayushporwal01 <porwalayush953@gmail.com>
2026-06-05 09:42:08 +0530

    Initial Commit

--- food-app/src/components/Cart.jsx
+++ food-app/src/components/Cart.jsx
@@ -1,17 +1,26 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Item from "./Item";
+import { clearCart } from "../utils/cartSlice";
+
+const dispatch = useDispatch();
+
+const handleClearCart = () => (
+  dispatch(clearCart())
+)
 
 export default function Cart() {
   const cartItems = useSelector((store) => store.cart.items);
 
   return (
-    <div className="mt-8">
+    <div className="mt-8 flex flex-col items-center">
       <h1 className="text-2xl text-center font-bold">Cart</h1>
       <div className="w-6/12 mx-auto">
         {cartItems.map((item, index) => (
           <Item index={index} item={item} />
         ))}
       </div>
+
+      <button className="w-30 h-10 bg-black text-white text-xl rounded-md cursor-pointer" onClick={handleClearCart}>Clear Cart</button>
     </div>
   );
 }
