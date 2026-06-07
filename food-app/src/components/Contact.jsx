dbc2894a8aa904ff7db373cbfea2b14a76b94174
ayushporwal01 <porwalayush953@gmail.com>
2026-06-07 13:04:32 +0530

    Initial Commit

--- food-app/src/components/Contact.jsx
+++ food-app/src/components/Contact.jsx
@@ -1,7 +1,22 @@
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Us</h1>;
+    <div className="mt-10 flex flex-col items-center">
+      <h1 className="text-2xl font-semibold text-center">Contact Us</h1>
+
+      <form action="#">
+        <div className="mt-8 space-x-3">
+          <label htmlFor="fullName" className="text-xl">
+            Full Name
+          </label>
+          <input type="text" id="fullName" className="border h-8" />
+        </div>
+        <div className="mt-8 space-x-3">
+          <label htmlFor="email" className="text-xl">
+            Email
+          </label>
+          <input type="text" id="email" className="border h-8" />
+        </div>
+      </form>
     </div>
   );
 }
