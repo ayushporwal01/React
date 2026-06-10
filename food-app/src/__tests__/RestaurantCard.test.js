e149bbc5aee00ce51e934baf27c146584c9ec8f8
ayushporwal01 <porwalayush953@gmail.com>
2026-06-10 19:22:50 +0530

    Initial Commit

--- food-app/src/__tests__/RestaurantCard.test.js
+++ food-app/src/__tests__/RestaurantCard.test.js
@@ -1,7 +1,11 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
 import RestaurantCard from "../components/RestaurantCard";
+import MOCK_DATA from "../mocks/resCardMock.json";
 
 it("Should render RestaurantCard with Data", () => {
-    render(<RestaurantCard resData={} />)
+  render(<RestaurantCard resData={MOCK_DATA} />);
 
-})
+  const name = screen.getByText("Pizza Hut");
+
+  expect(name).toBeInTheDocument();
+});
