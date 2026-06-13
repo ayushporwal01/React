2d9fb85d96a186976ffd84bac0f5a6dbc83e520f
ayushporwal01 <porwalayush953@gmail.com>
2026-06-13 10:38:13 +0530

    Initial Commit

---
+++ food-app/src/__tests__/Cart.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react"
+import RestaurantMenu from "../components/RestaurantMenu";
+
+it("Should render Restaurant Menu Component", () => {
+    render(<RestaurantMenu />);
+})
